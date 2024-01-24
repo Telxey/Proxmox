df9d159ce4069998ce6594ed1916e97cd1fa2000
Telxey <131807761+Telxey@users.noreply.github.com>
2024-01-24 12:04:12 -0500

    Create themes.sh

---
+++ MOX-Themes/themes.sh
@@ -0,0 +1,12 @@
+#! /bin/env bash
+
+apt install python3-pip python3.11-venv -y
+
+#virtual env
+python3 -m venv env
+source env/bin/activate
+pip3 install -r requirements.txt
+
+python3 PVThemes.py
+
+deactivate
