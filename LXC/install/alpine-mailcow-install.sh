cefbd343b23d5d29384861b1bf24738ac22c6c3c
Telxey <131807761+Telxey@users.noreply.github.com>
2024-07-13 07:49:34 -0400

    Create alpine-mailcow-install.sh

---
+++ LXC/install/alpine-mailcow-install.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2024 Telxey
+# Author: Rax (Raxon)
+# License: MIT
+# https://raw.githubusercontent.com/Telxey/Proxmox/main/LICENSE
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD apk add newt
+$STD apk add curl
+$STD apk add openssh
+$STD apk add tzdata
+$STD apk add nano
+$STD apk add mc
+$STD apk add wget
+$STD apk add ca-certificates
+$STD apk add git
+$STD apk add gnupg
+$STD apk add btop
+$STD apk add lsblk
+$STD apk add bind-tools
+msg_ok "Installed Dependencies"
+
+msg_info "Installing Docker"
+$STD apk add docker
+$STD apk add docker-compose
+$STD rc-service docker start
+$STD rc-update add docker default
+msg_ok "Installed Docker"
+
+get_latest_release() {
+  curl -sL https://api.github.com/repos/$1/releases/latest | grep '"tag_name":' | cut -d'"' -f4
+}
+PORTAINER_LATEST_VERSION=$(get_latest_release "portainer/portainer")
+DOCKER_COMPOSE_LATEST_VERSION=$(get_latest_release "docker/compose")
+
+read -r -p "Would you like to add Docker Compose? <y/N> " prompt
+if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+  msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+fi
+read -r -p "Would you like to add Portainer? <y/N> " prompt
+if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Portainer $PORTAINER_LATEST_VERSION"
+  docker volume create portainer_data >/dev/null
+  $STD docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name=portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+  msg_ok "Installed Portainer $PORTAINER_LATEST_VERSION"
+  fi
+fi  
+read -r -p "Would you like to add Docker Compose? <y/N> " prompt
+if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+  DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+  mkdir -p $DOCKER_CONFIG/cli-plugins
+  curl -sSL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_LATEST_VERSION/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+  chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+  msg_ok "Installed Docker Compose $DOCKER_COMPOSE_LATEST_VERSION"
+fi
+read -r -p "Would you like to add Docker Compose? <y/N> " prompt
+if [[ "${prompt,,}" =~ ^(y|yes)$ ]]; then
+  msg_info "Installing Docker Compose
+  mkdir -p /opt/{dockge,stacks}
+  wget -q -O /opt/dockge/compose.yaml https://raw.githubusercontent.com/louislam/dockge/master/compose.yaml
+  cd /opt/dockge
+  docker compose up -d
+  msg_ok "Installed Dockge"
+ fi
+fi  <-- Fix nneded
+
+motd_ssh
+customize
